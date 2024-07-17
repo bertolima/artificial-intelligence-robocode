df0817fdb9ef17964b44eb57f504aafbe4342c6a
bertolima <bertolilima27@gmail.com>
2024-07-17 11:54:42 -0300

    initial commit

---
+++ Fuzzy logic and Bayesian network/src/Enemy/EnemyBot.java
@@ -0,0 +1,149 @@
+package Enemy;
+
+import robocode.ScannedRobotEvent;
+
+import java.awt.geom.Point2D;
+
+public class EnemyBot {
+    private double distance;
+    private double bearing;
+    private double bearingRadians;
+    private double energy;
+    private double heading;
+    private double headingRadians;
+    private String name;
+    private double velocity;
+    private double distanceByCoords;
+    private double prevHeading;
+    private double prevHeadingRadians;
+    private double prevEnergy;
+    private Point2D.Double prevCoord;
+    private Point2D.Double coord;
+
+    public EnemyBot(){
+        reset();
+    }
+
+    public void reset(){
+        this.name = "";
+        this.bearing = 0d;
+        this.distance = 0d;
+        this.energy = 0d;
+        this.prevHeading = 0d;
+        this.velocity = 0d;
+        this.prevHeading = 0d;
+        this.heading = 0d;
+        this.headingRadians = 0d;
+        this.bearingRadians = 0d;
+        this.prevHeadingRadians = 0d;
+        this.distanceByCoords = 0d;
+        this.prevHeading = 0d;
+        this.prevHeadingRadians = 0d;
+        this.energy = 0d;
+        this.coord = new Point2D.Double(0d,0d);
+        this.prevCoord = new Point2D.Double(0d,0d);
+    }
+
+    public void update(ScannedRobotEvent ev){
+        this.name = ev.getName();
+        this.prevHeading = new Double(this.heading);
+        this.prevHeadingRadians = new Double(this.headingRadians);
+        this.headingRadians = ev.getHeadingRadians();
+        this.bearingRadians = ev.getBearingRadians();
+        this.bearing = ev.getBearing();
+        this.distance = ev.getDistance();
+        this.prevEnergy = new Double(this.energy);
+        this.energy = ev.getEnergy();
+        this.velocity = ev.getVelocity();
+        this.heading = ev.getHeading();
+    }
+
+    public double getPrevEnergy() {
+        return prevEnergy;
+    }
+
+    public void setPrevDistance(double d) {
+        this.distanceByCoords = d;
+    }
+
+    public void setPrevHeading(double prevHeading) {
+        this.prevHeading = prevHeading;
+    }
+
+    public void setPrevHeadingRadians(double prevHeadingRadians) {
+        this.prevHeadingRadians = prevHeadingRadians;
+    }
+
+    public void setPrevCoord(double x, double y){
+        this.prevCoord.setLocation(x,y);
+    }
+
+    public Point2D.Double getPrevCoord(){
+        return this.prevCoord;
+    }
+
+    public void setCoord(double x, double y){
+        this.coord.setLocation(x,y);
+    }
+
+    public Point2D.Double getCoord(){
+        return this.coord;
+    }
+
+    public double getX(){
+        return this.coord.getX();
+    }
+
+    public double getY(){
+        return this.coord.getY();
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getBearing() {
+        return bearing;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public double getHeading() {
+        return heading;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public double getPrevDistance() {
+        return distanceByCoords;
+    }
+
+    public double getPrevHeading() {
+        return prevHeading;
+    }
+
+    public double getBearingRadians() {
+        return bearingRadians;
+    }
+
+    public double getHeadingRadians() {
+        return headingRadians;
+    }
+
+    public double getPrevHeadingRadians() {
+        return prevHeadingRadians;
+    }
+
+    public boolean none(){
+        return this.name.equals("");
+    }
+
+}
---
+++ Fuzzy logic and Bayesian network/src/edo/FatorIntegrante.java
@@ -0,0 +1,290 @@
+package edo;
+
+import Enemy.EnemyBot;
+import robocode.*;
+
+import robocode.util.Utils;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+public class FatorIntegrante extends AdvancedRobot {
+
+    enum Signal {
+        PLUS, MINUS, NEUTRAL, NONE
+    }
+
+    enum Axis {
+        X, Y, NEUTRAL, NONE
+    }
+
+
+    /* Variaveis de controle de ações do robo */
+    private final double limitFromWall = 50d;
+    private final double maxDistanceBetweenRobots = 100d;
+    private final double marginFromWall = 10d;
+    private final double angleBeetweenEnemy = 90d;
+    private final double circularSpeed = 100d;
+    private Point2D.Double myCoord;
+    private int direction = 1;
+
+    /* Variaveis de parâmetros para calculos */
+    private double angleToEnemy;
+    private double newRadarAngle;
+    private double newAngleGun;
+    private double projection;
+    private Signal signal;
+
+    /* Variaveis de armazenamento de informações externas ao robo*/
+    private EnemyBot enemy = new EnemyBot();
+    private Point2D.Double myPrevCoord = new Point2D.Double(0d,0d);
+    private double distanceBeetweenMyCoords;
+    private double accourate = 0d;
+    private double totalShots = 0d;
+    private double hits = 0d;
+
+
+    public void run() {
+        setColors(Color.BLACK, Color.RED, Color.RED, Color.black, Color.GREEN);
+        setAdjustRadarForGunTurn(true);
+        setAdjustRadarForRobotTurn(true);
+        setAdjustGunForRobotTurn(true);
+        addCustomEvent(new Condition("nearWall") {
+            public boolean test() {
+                return (getX() <= limitFromWall || getX() >= getBattleFieldWidth() - limitFromWall  || getY() <= limitFromWall || getY() >= getBattleFieldHeight() - limitFromWall);}
+        });
+        setTurnRadarRight(Double.POSITIVE_INFINITY);
+        this.myCoord = new Point2D.Double(getX(), getY());
+
+        while (true) {
+            execute();
+            scan();
+            Point2D.Double newCoord = this.calculateMyNewCoords();
+            boolean flag = this.needNormalize(newCoord, this.marginFromWall);
+            if (flag) this.moveSet(this.normalizeCoords(newCoord, this.marginFromWall));
+            else this.moveSet(newCoord);
+        }
+    }
+
+    public void onScannedRobot (ScannedRobotEvent ev) {
+        if (this.enemy.none() || ev.getName().equals(enemy.getName())) enemy.update(ev);
+        this.calculateEnemyCoords();
+        this.lockIn();
+        this.chooseTargeting();
+    }
+
+    public void onCustomEvent(CustomEvent e) {
+
+    }
+
+    public void onRobotDeath(RobotDeathEvent event){
+        this.enemy.reset();
+        setTurnRadarRight (Double.POSITIVE_INFINITY);
+    }
+
+    public void onBulletHit(BulletHitEvent b){
+        this.hits++;
+        this.totalShots++;
+    }
+
+    public void onBulletMissed(BulletMissedEvent b){
+        this.totalShots++;
+    }
+
+    //calcula posição atual do robo inimigo
+    private void calculateEnemyCoords(){
+        this.enemy.setPrevCoord(this.enemy.getX(), this.enemy.getY());
+        this.myPrevCoord.setLocation(getX(), getY());
+        double enemyX = getX() + this.enemy.getDistance() * Math.sin(this.angleToEnemy);
+        double enemyY = getY() + this.enemy.getDistance() * Math.cos(this.angleToEnemy);
+        this.enemy.setCoord(enemyX, enemyY);
+    }
+
+    //calcula posição em que eu quero posicionar o meu robo com base no robo inimigo
+    private Point2D.Double calculateMyNewCoords(){
+        double myNewX = getX();
+        double myNewY = getY();
+        if (!this.enemy.none()) {
+            myNewX = this.enemy.getX() + Math.cos(angleBeetweenEnemy) * maxDistanceBetweenRobots;
+            myNewY = this.enemy.getY() + Math.sin(angleBeetweenEnemy) * maxDistanceBetweenRobots;
+        }
+        return new Point2D.Double(myNewX, myNewY);
+    }
+
+    //normaliza uma coordenada para que o robo nao colida com as paredes
+    private Point2D.Double normalizeCoords(Point2D.Double p, double offset){
+        p.x = Math.min(Math.max(18.0 + offset, p.x), getBattleFieldWidth() - 18.0 - offset);
+        p.y = Math.min(Math.max(18.0 + offset, p.y), getBattleFieldHeight() - 18.0 - offset);
+        return p;
+    }
+
+    private boolean needNormalize(Point2D.Double p, double offset){
+        return (p.x < 18.0 + offset
+                || p.y < 18.0 + offset
+                || p.x > getBattleFieldWidth() - 18.0 - offset
+                || p.y > getBattleFieldHeight() - 18.0 - offset);
+    }
+
+    //fixa a mira em um robo especifico
+    public void lockIn (){
+        this.angleToEnemy = this.getHeadingRadians() + this.enemy.getBearingRadians();
+        this.newRadarAngle = this.angleToEnemy - this.getRadarHeadingRadians();
+        setTurnRadarRightRadians(Utils.normalRelativeAngle(newRadarAngle));
+    }
+
+    private void goTo(Point2D.Double coord) {
+        coord.x = coord.x - this.getX();
+        coord.y = coord.y - this.getY();
+        double goAngle = Utils.normalRelativeAngle(Math.atan2(coord.x, coord.y) - this.getHeadingRadians());
+        setTurnRightRadians(Math.atan(Math.tan(goAngle)));
+        setAhead(Math.cos(goAngle) * Math.hypot(coord.x, coord.y));
+    }
+
+    private void circularMove(){
+        this.setTurnRight(this.enemy.getBearing() + this.angleBeetweenEnemy);
+        if (predictMyCircularMove(50)) setAhead(50);
+        else setBack(50);
+    }
+
+    private void moveSet(Point2D.Double coord){
+        if (!this.enemy.none()){
+
+            if (this.enemy.getDistance() < 200){
+                this.circularMove();
+            }
+            else this.goTo(coord);
+
+        }
+    }
+
+    private void chooseTargeting(){
+        double firePower = calculateFirePower();
+        double currentHeading = this.enemy.getHeadingRadians();
+        double headingDiff = currentHeading - this.enemy.getPrevHeadingRadians();
+        this.enemy.setPrevHeadingRadians(currentHeading);
+
+        double bulletSpeed = Rules.getBulletSpeed(firePower);
+        double travellingBulletTime;
+
+        Point2D.Double target;
+
+        if (Math.abs(headingDiff) > 0.01d){
+            travellingBulletTime  = this.enemy.getDistance()/bulletSpeed;
+            target = (Point2D.Double) this.predictCircularMove(this.enemy.getCoord(), this.enemy.getVelocity(), currentHeading, headingDiff, travellingBulletTime, firePower);
+        }
+        else{
+            travellingBulletTime = (this.enemy.getDistance() + this.enemy.getPrevDistance())/bulletSpeed;
+            target = linearTargeting(travellingBulletTime);
+        }
+
+        double currentAngle = Math.atan2(this.enemy.getY()-this.getY(), this.enemy.getX()-this.getX());
+        double futureAngle = Math.atan2(target.getY()-this.getY(), target.getX()-this.getX());
+
+        //double angle =  Utils.normalAbsoluteAngle(Math.atan2(target.x - getX(), target.y - getY()));
+
+        this.projection = futureAngle-currentAngle;
+        this.newAngleGun = this.angleToEnemy - this.getGunHeadingRadians();
+
+        //setTurnGunRightRadians(Utils.normalRelativeAngle(angle - getGunHeadingRadians()));
+        setTurnGunRightRadians(Utils.normalRelativeAngle(newAngleGun - projection));
+        setFire(firePower);
+        this.accourate = this.totalShots/this.hits;
+    }
+
+
+    private Point2D predictCircularMove(Point2D p, double speed, double currentHeading, double diffHeading, double time, double firePower){
+        Point2D.Double newCoords = new Point2D.Double(p.getX(), p.getY());
+        double delta = 0d;
+        while ((++delta) * time < Point2D.Double.distance(getX(), getY(), newCoords.x, newCoords.y)){
+
+            newCoords.x += Math.sin(currentHeading) * speed;
+            newCoords.y += Math.cos(currentHeading) * speed;
+            currentHeading += diffHeading;
+
+            if (this.needNormalize(newCoords, 0)){
+                newCoords.setLocation(this.normalizeCoords(newCoords, 0));
+                break;
+            }
+        }
+        return newCoords;
+    }
+
+    public Point2D.Double linearTargeting(double bulletTime){
+        Point2D.Double newCoords = new Point2D.Double(this.enemy.getX(), this.enemy.getY());
+        Axis axis = determineLinearMovement();
+        double projection = 2 * (this.enemy.getVelocity() * bulletTime);
+
+        if (this.signal.equals(Signal.MINUS)) projection *= -1;
+
+        if (axis.equals(Axis.X)) {
+            newCoords.setLocation(newCoords.getX() + projection, newCoords.getY());
+        } else if (axis.equals(Axis.Y)) {
+            newCoords.setLocation(newCoords.getX(), newCoords.getY() + projection);
+        } else if(axis.equals(Axis.NEUTRAL)){
+            newCoords.setLocation(newCoords.getX() + Math.cos(this.enemy.getHeading())*projection,
+                    newCoords.getY()+ Math.sin(this.enemy.getHeading())*projection);
+        }
+        this.signal = Signal.NONE;
+        return newCoords;
+    }
+
+    public Axis determineLinearMovement(){
+        Point2D.Double coordDiff = new Point2D.Double(this.enemy.getX() - this.enemy.getPrevCoord().getX(),
+                this.enemy.getY() - this.enemy.getPrevCoord().getY());
+        this.enemy.setPrevDistance(this.enemy.getCoord().distance(this.enemy.getPrevCoord()));
+        this.distanceBeetweenMyCoords = this.myPrevCoord.distance(getX(), getY());
+        if (Math.abs(Math.abs(coordDiff.x) - Math.abs(coordDiff.y)) > this.enemy.getPrevDistance()/2){
+            if (Math.max(Math.abs(coordDiff.x), Math.abs(coordDiff.y)) == Math.abs(coordDiff.x)){
+                if (coordDiff.x < 0) this.signal = Signal.MINUS;
+                else this.signal = Signal.PLUS;
+                return Axis.X;
+            }
+            else{
+                if (coordDiff.y < 0) this.signal = Signal.MINUS;
+                else this.signal = Signal.PLUS;
+                return Axis.Y;
+            }
+        } else{
+            this.signal = Signal.NEUTRAL;
+            return Axis.NEUTRAL;
+        }
+    }
+
+    public double calculateFirePower(){
+        double fp;
+        if (this.getEnergy() < 10d) fp = 0.1d;
+        else if(this.enemy.getDistance() > 400) fp = 1.5d;
+        else if (this.enemy.getDistance() < 200) fp = 3d;
+        else fp = 2d;
+        return Math.min(this.enemy.getEnergy()/4, fp);
+    }
+
+    public boolean predictMyCircularMove(double distance){
+        double speed = this.getVelocity();
+        double time = distance / speed;
+
+        Point2D.Double p = new Point2D.Double(getX(), getY());
+        double delta = 0d;
+        double heading = this.getHeadingRadians();
+        double prevHeading = 0d;
+        double diffHeading = 0d;
+        while (Math.abs((++delta) * speed) < distance){
+            if (Utils.isNear(getTime() % 16, 0d)) this.direction *= -1;
+            if (direction == -1) return false;
+            if (Utils.isNear(speed, 0d)) return false;
+            p.x += Math.cos(heading) * speed;
+            p.y += Math.sin(heading) * speed;
+            diffHeading = heading - prevHeading;
+            prevHeading = heading;
+            heading += diffHeading;
+
+            out.println(heading + "  " + diffHeading + " " + prevHeading);
+            out.println("distance: " + delta*speed + " " + delta + " " + speed);
+            if (needNormalize(p, 0d)) return false;
+        }
+        if (needNormalize(p, 0d)) return false;
+        return true;
+
+    }
+
+}
